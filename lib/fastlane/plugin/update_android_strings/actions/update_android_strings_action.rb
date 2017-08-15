6685ce8b3c1f56c1c715c37a7be90534f0c5268e
bang <sqibang@gmail.com>
2017-08-15 17:34:20 +0700

    feat: done

--- lib/fastlane/plugin/update_android_strings/actions/update_android_strings_action.rb
+++ lib/fastlane/plugin/update_android_strings/actions/update_android_strings_action.rb
@@ -2,7 +2,53 @@ module Fastlane
   module Actions
     class UpdateAndroidStringsAction < Action
       def self.run(params)
-        UI.message("The update_android_strings plugin is working!")
+        require 'ox'
+
+        if params[:block]
+          if params[:xml_path]
+            xmlPath = params[:xml_path]
+          else
+            xmlPath = "android/app/src/main/res/values/strings.xml"
+          end
+          unless File.exist?(xmlPath)
+            UI.user_error!("The xml file at path `#{xmlPath}` doesn't exist.")
+          end
+          f = File.open(xmlPath, "r")
+          xml = f.read
+          f.close
+
+          strings = Ox.parse(xml)
+
+          unless strings
+            UI.error("xml is Empty!")
+            return
+          end
+          # find key and update text with new value
+          strings.nodes.each do |element|
+            if element.respond_to? :name and element["name"] == "reactNativeCodePush_androidDeploymentKey"
+              element.replace_text("hello")
+            end
+          end
+					if params[:block]
+						hashes = {}
+            params[:block].call(hashes)
+
+            Helper::UpdateAndroidStringsHelper.updateXml(strings.nodes, hashes)
+
+            # dump new xml file
+            newXml = Ox.dump(strings)
+            if xml != newXml
+              File.write(xmlPath, newXml)
+              UI.success("Update xml file success.")
+            else
+              UI.message("Nothing has changed.")
+            end
+          end
+
+        else
+          UI.important("You haven't specified block parameter to update your xml file")
+          false
+        end
       end
 
       def self.description
@@ -24,11 +70,14 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "UPDATE_ANDROID_STRINGS_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+          FastlaneCore::ConfigItem.new(key: :xml_path,
+        env_name: "UPDATE_ANDROID_STRINGS_XML_PATH",
+        description: "Path to strings.xml",
+        optional: true),
+          FastlaneCore::ConfigItem.new(key: :block,
+                               description: "A block to process strings.xml with custom logic",
+                                  optional: false,
+                                  is_string: false)
         ]
       end
 
@@ -37,6 +86,7 @@ module Fastlane
         # See: https://github.com/fastlane/fastlane/blob/master/fastlane/docs/Platforms.md
         #
         # [:ios, :mac, :android].include?(platform)
+        [:android].include?(platform)
         true
       end
     end
--- lib/fastlane/plugin/update_android_strings/helper/update_android_strings_helper.rb
+++ lib/fastlane/plugin/update_android_strings/helper/update_android_strings_helper.rb
@@ -1,11 +1,20 @@
+
 module Fastlane
   module Helper
     class UpdateAndroidStringsHelper
       # class methods that you define here become available in your action
       # as `Helper::UpdateAndroidStringsHelper.your_method`
       #
-      def self.show_message
-        UI.message("Hello from the update_android_strings plugin helper!")
+			def self.updateXml(nodes, hashes = {})
+				if hashes and nodes
+					# find key and update text with new value
+          nodes.each do |element|
+            if element.respond_to? :name and hashes.has_key?(element["name"])
+							element.replace_text(hashes.fetch(element["name"]))
+            end
+					end
+				end
+
       end
     end
   end
